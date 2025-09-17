1cac3c7f431cf0fa43522ee31597fa60718b34e5
JThilinaDK123 <JThilinaDK123@gmail.com>
2025-09-17 11:46:26 +0530

    final commit

--- application.py
+++ application.py
@@ -102,7 +102,7 @@ def metrics():
     return Response(generate_latest() , content_type='text/plain')
     
 if __name__ =="__main__":
-    start_http_server(8000)
-    app.run(debug=True , host='0.0.0.0' , port = 5000)
+    # start_http_server(8002)
+    app.run(debug=True , host='0.0.0.0' , port = 5001)
 
 
